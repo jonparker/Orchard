a5caaae684126926996f878ae4bb900c6718debc
Richard Smith <richard.smith@worldvision.com.au>
2012-07-06 17:24:41 +1000

    init

---
+++ src/Orchard.Web/Modules/maps/Drivers/MapDriver.cs
@@ -0,0 +1,38 @@
+﻿using Maps.Models;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Drivers;
+
+namespace Maps.Drivers
+{
+    public class MapDriver : ContentPartDriver<MapPart>
+    {
+        protected override DriverResult Display(
+            MapPart part, string displayType, dynamic shapeHelper)
+        {
+
+            return ContentShape("Parts_Map", () => shapeHelper.Parts_Map(
+                Longitude: part.Longitude,
+                Latitude: part.Latitude));
+        }
+
+        //GET
+        protected override DriverResult Editor(
+            MapPart part, dynamic shapeHelper)
+        {
+
+            return ContentShape("Parts_Map_Edit",
+                () => shapeHelper.EditorTemplate(
+                    TemplateName: "Parts/Map",
+                    Model: part,
+                    Prefix: Prefix));
+        }
+        //POST
+        protected override DriverResult Editor(
+            MapPart part, IUpdateModel updater, dynamic shapeHelper)
+        {
+
+            updater.TryUpdateModel(part, Prefix, null, null);
+            return Editor(part, shapeHelper);
+        }
+    }
+}
