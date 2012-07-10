e318cbea9066eb300c6a632d8ae52013dcfc6307
Richard Smith <richard.smith@worldvision.com.au>
2012-07-10 17:36:39 +1000

    driver

---
+++ src/Orchard.Web/Modules/SkyWalker.WebShop/Drivers/ProductDriver.cs
@@ -0,0 +1,21 @@
+﻿using Orchard.ContentManagement.Drivers;
+
+namespace SkyWalker.WebShop.Drivers
+{
+    public class ProductDriver : ContentPartDriver<ProductPart>
+    {
+        //get
+        protected override DriverResult Editor(ProductPart part, dynamic shapeHelper)
+        {
+            return ContentShape("Parts_Product_Edit",
+                () => shapeHelper.EditorTemplate(TemplateName: "Parts/Product", Model: part, Prefix: Prefix));
+        }
+        //post
+        protected override DriverResult Editor(ProductPart part, Orchard.ContentManagement.IUpdateModel updater, dynamic shapeHelper)
+        {
+            updater.TryUpdateModel(part, Prefix, null, null);
+            return Editor(part, shapeHelper);
+        }
+
+    }
+}
