119f594b18a73f457a91835ff0e92a66970d9a07
Richard Smith <richard.smith@worldvision.com.au>
2012-07-10 11:10:24 +1000

    skywalker.webshop

---
+++ src/Orchard.Web/Modules/SkyWalker.WebShop/Data/Migration.cs
@@ -0,0 +1,18 @@
+﻿using Orchard.Data.Migration;
+
+namespace SkyWalker.WebShop.Data
+{
+    public class Migration : DataMigrationImpl
+    {
+        public int Create()
+        {
+            SchemaBuilder.CreateTable("ProductRecord", table => table
+                .ContentPartRecord()
+                .Column<decimal>("Price")
+                .Column<string>("Sku", column => column.WithLength(50))
+                );
+
+            return 1;
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/SkyWalker.WebShop/ProductPart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Orchard.ContentManagement;
+
+namespace SkyWalker.WebShop
+{
+    public class ProductPart : ContentPart<ProductRecord>
+    {
+        public decimal Price
+        {
+            get { return Record.Price; }
+        }
+        public string Sku
+        {
+            get { return Record.Sku; }
+        }
+    }
+
+    public class ProductRecord
+    {
+        public virtual decimal Price { get; set; }
+        public virtual string Sku { get; set; }
+
+    }
+}
